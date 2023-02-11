6f3b117da90ef14d5f034bee7495d15a893fa2a9
abdulla <abdullasahmad5@gmail.com>
2023-02-11 22:08:40 +0300

    first

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:books_info/screens/homepage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalCupertinoLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale("fa", "IR"), // OR Locale('ar', 'AE') OR Other RTL locales
+      ],
+      locale: Locale("fa", "IR"), // OR Locale('ar', 'AE') OR Other RTL locales,
+      routes: {('/'): (context) => HomePage()},
+    );
+  }
+}
